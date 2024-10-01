97005ae38976231cbd11468e3aad3523b045a5f9
huss-al <165154776+huss-al@users.noreply.github.com>
2024-10-01 13:07:10 +0000

    deploying to heroku

--- westside_projects/settings.py
+++ westside_projects/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=t6&wjk)nr_-@tued=dhf!9beska5v#fdhtyr92#h$fn0yw-o!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
